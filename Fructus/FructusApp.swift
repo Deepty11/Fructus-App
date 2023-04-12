88a81eaf1104d1948f32d042da54154c5fb3621b
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-04-12 09:04:09 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rehnuma Reza Deepty on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
